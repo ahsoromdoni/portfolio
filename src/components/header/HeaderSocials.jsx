1fcc703dc6d0450fae2a924d581877cd79319baa
Ahmad S Romdoni <ahmad.solehudin@bluebirdgroup.com>
2023-08-20 21:30:39 +0700

    fix linkedin url

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {FaInstagram} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="http://https://www.linkedin.com/in/ahsoromdoni" target='_blank'><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/ahsoromdoni" target='_blank'><BsLinkedin/></a>
         <a href="https://github.com/ahsoromdoni" target='_blank'><FaGithub/></a>
         <a href="https://www.instagram.com/romdonism/" target='_blank'><FaInstagram/></a>
     </div>
